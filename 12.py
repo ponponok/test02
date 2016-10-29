fb325ec20b74b8e91bf7926aefea43293fbc3705
EngInk <59070193@it.kmitl.ac.th>
2016-10-29 14:43:58 +0700

    123132123132

--- 12.py
+++ 12.py
@@ -2,7 +2,6 @@ def findArea():
     pi = 22/7
     r = 4
     area = pi * r * r
-    print("test")
     return area
 
 print(findArea())
---
+++ lol.py
@@ -0,0 +1,4 @@
+def lol():
+    for _ in range(100):
+    print("lol")
+lol()
