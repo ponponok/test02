4cac4245e7fac780c5a631b013ab73b81fd2f90d
ponponok <59070017@it.kmitl.ac.th>
2016-11-12 17:21:49 +0700

    add average function

--- psit.py
+++ psit.py
@@ -5,3 +5,12 @@ def main():
     file = open('sizee.txt', 'r')
     data = csv.reader(file)
     lst = [row for row in data]
+    avcom, avnet = average(lst, total_com, total_net)
+
+def average(lst, total_com, total_net):
+    averg = []
+    for com in lst:
+        averg.append([(int(com[2])/total_com*100), (int(com[3])/total_net*100)])
+    avcom = [com for com, net in averg]
+    avnet = [net for com, net in averg]
+    return avcom, avnet
