246022794ef4e80de6ea6a430b57634bae0c3cc4
ponponok <59070017@it.kmitl.ac.th>
2016-11-12 17:19:49 +0700

    add total function

--- psit.py
+++ psit.py
@@ -5,3 +5,13 @@ def main():
     file = open('sizee.txt', 'r')
     data = csv.reader(file)
     lst = [row for row in data]
+    total_bi = total(lst, 1)
+    total_com = total(lst, 2)
+    total_net = total(lst, 3)
+
+def total(lst, time):
+    count = 0
+    for business in lst:
+        count += int(business[time])
+    print(count)
+    return count
